7c4f1ff0eeebdf53ba7c08c4af8a5442908051e2
JaMeZ-B <j.bantje@wwu.de>
2016-03-09 21:07:42 +0100

    added another option
    Signed-off-by: JaMeZ-B <j.bantje@wwu.de>

--- compile_all.sh
+++ compile_all.sh
@@ -2,7 +2,7 @@
 declare -a arr=("Topo2_WS15/topologie_2" "Topo1_WS14/topologie_1" "PDGL1_SS14/PDGL1")
 for i in "${arr[@]}"
 do
-	latexmk -pdflatex="xelatex --shell-escape %O %S" -pdf -dvi- -ps- -cd -silent -f "$i.tex";
+	latexmk -pdflatex="xelatex --shell-escape %O %S" -pdf -dvi- -ps- -cd -silent -f -interaction=nonstopmode "$i.tex";
 done
 exit 0
 
