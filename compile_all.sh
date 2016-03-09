2901c2d47d48229d9ce32be2e1b9a68bd8f195e6
JaMeZ-B <j.bantje@wwu.de>
2016-03-09 21:46:31 +0100

    added LA2
    Signed-off-by: JaMeZ-B <j.bantje@wwu.de>

--- compile_all.sh
+++ compile_all.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
-declare -a arr=("Topo2_WS15/topologie_2" "Topo1_WS14/topologie_1" "PDGL1_SS14/PDGL1" "OpAlg_WS15/operatoralgebren")
+declare -a arr=("Topo2_WS15/topologie_2" "Topo1_WS14/topologie_1" "PDGL1_SS14/PDGL1" "OpAlg_WS15/operatoralgebren" "LA2_SS16/LA2")
 for i in "${arr[@]}"
 do
 	latexmk -pdflatex="xelatex --shell-escape %O %S" -pdf -dvi- -ps- -cd -silent -f -interaction=nonstopmode "$i.tex";
 done
+
+# declare -a arr2=("ModRed_WS15/Modellreduktion")
+# for i in "${arr2[@]}"
+# do
+# 	latexmk -pdflatex="pdflatex --shell-escape %O %S" -pdf -dvi- -ps- -cd -silent -f -interaction=nonstopmode "$i.tex";
+# done
 exit 0
 
