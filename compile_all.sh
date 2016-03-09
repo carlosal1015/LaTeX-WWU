00bbf69ae79692a3576f805181aeffdc732d0bf9
JaMeZ-B <j.bantje@wwu.de>
2016-03-09 21:01:48 +0100

    updated compile all script
    Signed-off-by: JaMeZ-B <j.bantje@wwu.de>

--- compile_all.sh
+++ compile_all.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-declare -a arr=("Topo2_WS15/topologie_2" "Topo1_WS14/topologie_1")
+declare -a arr=("Topo2_WS15/topologie_2" "Topo1_WS14/topologie_1" "PDGL1_SS14/PDGL1")
 for i in "${arr[@]}"
 do
-	latexmk -pdflatex="xelatex --shell-escape %O %S" -pdf -dvi- -ps- -cd -silent "$i.tex";
+	latexmk -pdflatex="xelatex --shell-escape %O %S" -pdf -dvi- -ps- -cd -silent -f "$i.tex";
 done
 exit 0
 
