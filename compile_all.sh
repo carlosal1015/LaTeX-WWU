a54edc812e945230b4eae581cc15b841aa9f3fce
JaMeZ-B <j.bantje@wwu.de>
2016-03-09 21:13:49 +0100

    added operatoralgebren
    Signed-off-by: JaMeZ-B <j.bantje@wwu.de>

--- compile_all.sh
+++ compile_all.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-declare -a arr=("Topo2_WS15/topologie_2" "Topo1_WS14/topologie_1" "PDGL1_SS14/PDGL1")
+declare -a arr=("Topo2_WS15/topologie_2" "Topo1_WS14/topologie_1" "PDGL1_SS14/PDGL1" "OpAlg_WS15/operatoralgebren")
 for i in "${arr[@]}"
 do
 	latexmk -pdflatex="xelatex --shell-escape %O %S" -pdf -dvi- -ps- -cd -silent -f -interaction=nonstopmode "$i.tex";
