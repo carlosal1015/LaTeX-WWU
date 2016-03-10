bb2dbbdfcebfadba3aa83f0a1c1e56d98c386e2d
JaMeZ-B <j.bantje@wwu.de>
2016-03-10 14:33:42 +0100

    added FunkAna
    Signed-off-by: JaMeZ-B <j.bantje@wwu.de>

--- compile_all.sh
+++ compile_all.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-declare -a arr=("Topo2_WS15/topologie_2" "Topo1_WS14/topologie_1" "PDGL1_SS14/PDGL1" "OpAlg_WS15/operatoralgebren" "LA2_SS16/LA2" "LA2_SS13/lineare_algebra2" "LA1_WS12/lineare_algebra1" "KTheorie_SS15/K-Theorie" "HoehAlg1_SS14/hoehere_algebra")
+declare -a arr=("Topo2_WS15/topologie_2" "Topo1_WS14/topologie_1" "PDGL1_SS14/PDGL1" "OpAlg_WS15/operatoralgebren" "LA2_SS16/LA2" "LA2_SS13/lineare_algebra2" "LA1_WS12/lineare_algebra1" "KTheorie_SS15/K-Theorie" "HoehAlg1_SS14/hoehere_algebra" "FunkAna_WS14/funktional_analysis")
 for i in "${arr[@]}"
 do
 	latexmk -pdflatex="xelatex --shell-escape %O %S" -pdf -dvi- -ps- -cd -silent -f -interaction=nonstopmode "$i.tex";
